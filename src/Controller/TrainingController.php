092b024a44d8a7a99bac4b920d3c0b046b403d1c
pvanmaren <302456388@student.rocmondriaan.nl>
2023-06-07 12:21:59 +0200

    php crash

--- src/Controller/TrainingController.php
+++ src/Controller/TrainingController.php
@@ -3,13 +3,18 @@
 namespace App\Controller;
 
 use App\Entity\Training;
+use App\Entity\User;
+use App\Form\RegisterType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Security;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+
 
 class TrainingController extends AbstractController
 
@@ -47,10 +52,35 @@ class TrainingController extends AbstractController
 
     }
     #[Route('/register', name: 'register')]
-    public function register(): Response
+    public function register(Request $request, EntityManagerInterface $em, UserPasswordHasherInterface $passwordHasher): Response
     {
 
-        return new Response('register');
+        $register = $em->getRepository(User::class)->findAll();
+        $user = new User(...);
+        $plaintextPassword = ...;
+          $hashedPassword = $passwordHasher->hashPassword(
+              $user,
+              $plaintextPassword);
+          $user->setPassword($hashedPassword);
+
+        $form = $this->createForm(RegisterType::class, $user);
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user = $form->getData();
+            $user->setProduct($register);
+            $em->persist($user);
+            $em->flush();
+
+            $this->addFlash('succes', 'Uw bestelling is doorgestuurd!');
+            //  return $this->redirectToRoute('app_index');
+        }
+
+        return $this->renderForm('training/register.html.twig', [
+            'form' => $form
+
+        ]);
 
 
     }
@@ -66,5 +96,7 @@ class TrainingController extends AbstractController
         return $this ->$this->redirectToRoute('app_default');
 
     }
+
+
 
 }
